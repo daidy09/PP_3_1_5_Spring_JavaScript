13b6a06d78b8238d9ed5527ec06a232e5fdb07cc
Dmitry <dziamentsyeu.dzmitry@gmail.com>
2023-02-18 19:26:49 +0300

    deleteUser

---
+++ demo/src/main/java/com/dementiev/demo/controllers/AdminRESTController.java
@@ -0,0 +1,67 @@
+package com.dementiev.demo.controllers;
+
+import com.dementiev.demo.model.User;
+import com.dementiev.demo.service.UserService;
+import com.dementiev.demo.service.UserServiceImpl;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/admins")
+public class AdminRESTController {
+    private final UserServiceImpl userService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AdminRESTController(UserServiceImpl userService, BCryptPasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> showUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> showUser(@PathVariable("id") Long id) {
+        return new ResponseEntity<> (userService.getUserById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/userAuth")
+    public ResponseEntity<User> showAuthUser() {
+        return new ResponseEntity<> (userService.getCurrentUser(), HttpStatus.OK);
+    }
+
+    @PostMapping("/newAddUser")
+    public ResponseEntity<HttpStatus> saveNewUser(
+            @RequestBody User user
+    ) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.saveUser(user);
+        return new ResponseEntity<> (HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") Long id) {
+        userService.getUserById(id);
+        return new ResponseEntity<> (HttpStatus.OK);
+    }
+
+
+    @PatchMapping("/users/{id}")
+    public ResponseEntity<HttpStatus> userSaveEdit(@RequestBody @NotNull User user, @PathVariable Long id) {
+        user.setId(id);
+        System.out.println(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.editUser(id, user);
+
+        return new ResponseEntity<> (HttpStatus.OK);
+    }
+}
